e3115d5b793887bc471a4f647107fd737aab2a6c
Rachit Kataria <rachitka@usc.edu>
2017-04-26 11:49:46 -0700

    Added functionality to download ACM bibtext

--- scrapyACMAbs/scrapyACMAbs/spiders/scrapyACMAbs_Spider.py
+++ scrapyACMAbs/scrapyACMAbs/spiders/scrapyACMAbs_Spider.py
@@ -3,6 +3,7 @@ from scrapy.selector import HtmlXPathSelector
 from scrapy.contrib.loader import XPathItemLoader
 
 import urlparse
+import webbrowser
 
 from scrapy.http import Request
 
@@ -32,8 +33,24 @@ class scrapyACMAbsSpider(BaseSpider):
             return text
 
         abstract = encodeString(response.xpath("//div[contains(@class, 'layout')]/div[2]/div//text()").extract())
+        bibtext = response.xpath("//meta[@name='citation_abstract_html_url']/@content")[0].extract()
 
-        print abstract
+        count = 0
+        for c in bibtext:
+            if(c.isdigit()):
+                bibtext = bibtext[count:]
+                break
+            else:
+                count+=1
+
+        parent_id = ""
+        for c in bibtext:
+            if not c.isdigit():
+                break
+            parent_id += c
+
+        bibtext_url = "http://dl.acm.org/downformats.cfm?id=" + self.id + "&parent_id=" + parent_id + "&expformat=bibtex"
+        webbrowser.open(bibtext_url)  # Open bibtext
 
         with open('./abstract.json', 'w') as outfile:
-            json.dump({'abstract': abstract}, outfile)
+            json.dump({'abstract': parent_id}, outfile)
